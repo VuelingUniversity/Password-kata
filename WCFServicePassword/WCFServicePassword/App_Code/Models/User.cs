bff0e52f8c0fdd0a95fdfd6365a7b3948a1bd81c
acizquierdo <acizquierdo@hiberus.com>
2021-10-05 21:18:00 +0200

    WCF Service

---
+++ WCFServicePassword/WCFServicePassword/App_Code/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+[DataContract]
+public class User
+{
+    string userName;
+    string password;
+	[DataMember(IsRequired =true)]
+	public string UserName
+	{
+		get { return userName; }
+		set { userName = value; }
+	}
+	[DataMember(IsRequired = true)]
+	public string Password
+	{
+		get { return password; }
+		set { password = value; }
+	}
+
+}
---
+++ WCFServicePassword/WCFServicePassword/App_Code/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Security;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+    public bool AreValidUserCredential(User user)
+    {
+        var user1 = new User();
+        if (user.UserName == user1.UserName && user.Password == user1.Password)
+        {
+           
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+ 
+}
