a967444f20c9184156c0e13fabdc55c13aa8983e
Selene <selene.ajtd@gmail.com>
2021-10-05 22:18:38 +0200

    EncriptionService test.

---
+++ Password.ServiceLibrary.Test/EncriptionServiceTest.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Password.Core.Models;
+using Password.Core.Services;
+using System;
+
+namespace Password.ServiceLibrary.Test
+{
+    [TestClass]
+    public class EncriptionServiceTest
+    {
+        [TestMethod]
+        public void ValidatePassword_Returns_True_When_Password_Is_Correct()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+            var fakeUser = new User { Name = "juan23", Email = "juan@gmail.com", Salt = "0xLJsHVXYsvekpbr", Password = "FB6979BB477A1805F4DC1BDDB3D0B6ED78ED930C" };
+            userRepository.Setup(repository => repository.GetUserByUsername("juan23")).Returns(fakeUser);
+            var encryptionService = new EncryptionService(userRepository.Object);
+            // Act
+            var result = encryptionService.ValidatePassword("juan23", "juan1234");
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ValidatePassword_Returns_False_When_User_Does_Not_Exist()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+            var encryptionService = new EncryptionService(userRepository.Object);
+            // Act
+            var result = encryptionService.ValidatePassword("juan23", "juan1234");
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ValidatePassword_Returns_False_When_Password_Is_Wrong()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+            var fakeUser = new User { Name = "juan23", Email = "juan@gmail.com", Salt = "0xLJsHVXYsvekpbr", Password = "FB6979BB477A1805F4DC1BDDB3D0B6ED78ED930C" };
+            userRepository.Setup(repository => repository.GetUserByUsername("juan23")).Returns(fakeUser);
+            var encryptionService = new EncryptionService(userRepository.Object);
+            // Act
+            var result = encryptionService.ValidatePassword("juan23", "password");
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void EncryptPassword_Returns_As_Expected()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+            var encryptionService = new EncryptionService(userRepository.Object);
+            // Act
+            var hashPassword = encryptionService.EncryptPassword("juan1234", "0xLJsHVXYsvekpbr");
+            // Assert
+            hashPassword.Should().Be("FB6979BB477A1805F4DC1BDDB3D0B6ED78ED930C");
+        }
+
+        [TestMethod]
+        public void EncryptPassword_Does_Not_Work_Without_Salt()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+            var encryptionService = new EncryptionService(userRepository.Object);
+            // Act
+            var hashPassword = encryptionService.EncryptPassword("juan1234", "");
+            // Assert
+            hashPassword.Should().NotBe("FB6979BB477A1805F4DC1BDDB3D0B6ED78ED930C");
+        }
+    }
+}
---
+++ Password.ServiceLibrary.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Password.ServiceLibrary.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Password.ServiceLibrary.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64018545-7cf7-4487-8777-850fea4ccba6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
