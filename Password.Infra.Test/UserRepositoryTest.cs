4bf8904352493b1be5a88b2e1fd953d10ff6fdbb
Selene <selene.ajtd@gmail.com>
2021-10-06 18:14:15 +0200

    Repository tests.

--- Password.Infra.Test/UserRepositoryTest.cs
+++ Password.Infra.Test/UserRepositoryTest.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Password.Core.Models;
 using Password.Core.Services;
 using System;
 
@@ -8,24 +10,49 @@ namespace Password.Infra.Test
     [TestClass]
     public class UserRepositoryTest
     {
-        [TestMethod]
-        public void GetUserByUsername_Returns_User()
+        private IUserRepository _userRepository;
+
+        [TestInitialize]
+        public void GetSUT()
         {
-            // Arrange
-
-            // Act
-
-            // Assert
+            _userRepository = new UserRepository();
         }
 
         [TestMethod]
-        public void GetUserByUsername_Returns_Null_With_NotFound_User()
+        public void When_Username_Exists_GetUserByUsername_Returns_User()
         {
-            // Arrange
-
             // Act
-
+            var user = _userRepository.GetUserByUsername("pepa12");
             // Assert
+            user.Should().BeOfType<User>();
+            user.Name.Should().Be("pepa12");
+        }
+
+        [TestMethod]
+        public void When_Username_Does_Not_Exist_GetUserByUsername_Returns_Null()
+        {
+            // Act
+            var user = _userRepository.GetUserByUsername("juanita");
+            // Assert
+            user.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void When_Email_Exits_IsEmailValid_Returns_True()
+        {
+            // Act
+            var result = _userRepository.IsEmailValid("pepa@gmail.com");
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void When_Email_Does_Not_Exit_IsEmailValid_Returns_False()
+        {
+            // Act
+            var result = _userRepository.IsEmailValid("hola@gmail.com");
+            // Assert
+            result.Should().BeFalse();
         }
     }
 }
